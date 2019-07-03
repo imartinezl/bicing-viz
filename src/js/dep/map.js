8da444bb7f0fed6e720ce7e4b1faf813c8ff1c9a
imartinezl <inigomlap@gmail.com>
2019-07-03 23:35:36 +0200

    Mapbox style + map zoom

--- src/js/dep/map.js
+++ src/js/dep/map.js
@@ -1,9 +1,9 @@
 const options = {
   lat: 41.3977,
   lng:  2.1606,
-  zoom: 13,
+  zoom: 14,
   studio: true,
-  style: 'mapbox://styles/inigoml/cjolr9fcq1x352rpilptsjt6c'
+  style: 'mapbox://styles/inigoml/cjq0uk62956tg2roayaaxkocg' //'mapbox://styles/inigoml/cjolr9fcq1x352rpilptsjt6c'
 }
 const key = 'pk.eyJ1IjoiaW5pZ29tbCIsImEiOiJjamcycndxcDAwcmlsMnFwaHk4eDdpanhnIn0.lOge1jvtZgNLhr6yUdz8qA'
 const mappa = new Mappa('Mapbox', key);
