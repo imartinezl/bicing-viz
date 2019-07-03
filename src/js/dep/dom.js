6582c17d0bc3a6b1c65b6d89b11b2ff6759eaf7b
imartinezl <inigomlap@gmail.com>
2019-07-03 23:35:09 +0200

    DOM styling + text + div container

--- src/js/dep/dom.js
+++ src/js/dep/dom.js
@@ -2,11 +2,25 @@ let d=-1;
 let fSlider, tSlider, button;
 
 function initPlayButton(){
+  elem = createDiv('\n')
+  elem.position(1230,540);
+  elem.style('width', '365px');
+  elem.style('height', '165px');
+  elem.style('padding', '20px');
+  elem.style('background-color', '#f0f0f0');
+
   button = createButton('START/STOP');
   //button = createCheckbox('START/STOP', true);
-  button.position(20,150);
-  button.style('width', '300px');
-  button.style('z-index', '1');
+  button.position(1250,675);
+  button.style('width', '365px');
+  button.style('background-color', '#dfb04b');
+  button.style('border', 'none');
+  button.style('color', '#161616');
+  button.style('padding', '15px');
+  button.style('font-family', 'PT Sans');
+  button.style('font-size', '14pt');
+
+  //button.style('z-index', '1');
   button.mousePressed(buttonMousePressed)
 }
 function buttonMousePressed(){
@@ -14,10 +28,18 @@ function buttonMousePressed(){
 }
 
 function initFreqSlider(){
+  freq_text = createP('Speed');
+  freq_text.position(1250,540);
+  freq_text.style('font-size', '14pt');
+  freq_text.style('font-family', 'PT Sans');
+  freq_text.style('font-style', 'bold');
+  freq_text.style('color', '#161616');
+  //freq_text.style('text-shadow', '1px 1px #c2c2c2');
+
   fSlider = createSlider(0.5, 15, f/60, 0.5);
-  fSlider.position(20,100);
+  fSlider.position(1310,555);
   fSlider.style('width', '300px');
-  fSlider.style('z-index', '1');
+  //fSlider.style('z-index', '1');
   fSlider.changed(fSliderChanged);
 }
 function fSliderChanged(){
@@ -34,10 +56,19 @@ function fSliderChanged(){
    initTimeSlider();
 }
 function initTimeSlider(){
+  time_text = createP('Time');
+  time_text.position(1250,600);
+  time_text.style('font-size', '14pt');
+  time_text.style('font-family', 'PT Sans');
+  time_text.style('font-style', 'bold');
+  time_text.style('color', '#161616');
+  //time_text.style('text-shadow', '1px 1px #c2c2c2');
+
+
   tSlider = createSlider((startTMin-1)*f, stopTMax*f, t, g); // in seconds
-  tSlider.position(20,200);
+  tSlider.position(1310,615);
   tSlider.style('width', '300px');
-  tSlider.style('z-index', '1');
+  //tSlider.style('z-index', '1');
   tSlider.changed(tSliderChanged);
 }
 let tSliderPulsed = -1;
